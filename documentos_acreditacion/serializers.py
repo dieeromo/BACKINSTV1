a6a41eb6df47f2b76e54072a910d57f4549e3e31
Diego <diegoromo_408@hotmail.com>
2024-01-23 19:38:26 -0500

    prueba pull 1

--- documentos_acreditacion/serializers.py
+++ documentos_acreditacion/serializers.py
@@ -7,7 +7,7 @@ class documentos_acreditacion_Serializer(serializers.ModelSerializer):
     #digitador = serializers.CharField(source='digitador.first_name ', read_only=True)
     responsable = serializers.CharField(source='responsable.get_full_name', read_only=True)
     responsable_id = serializers.CharField(source='responsable.id', read_only=True)
-   
+    # este es un comentario de prueba
     #responsablef = serializers.CharField(source='responsable.last_name', read_only=True)
     criterio = serializers.CharField(source='criterio.criterio', read_only=True)
     subCriterio = serializers.CharField(source='subCriterio.subCriterio', read_only=True)
