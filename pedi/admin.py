9943f064e8a210bdca08e18a4ad2afe2f256b5e1
Diego <diegoromo_408@hotmail.com>
2025-03-07 15:17:58 -0500

    se agrega columna en administracion de django

--- pedi/admin.py
+++ pedi/admin.py
@@ -14,7 +14,7 @@ admin.site.register(Medio_verificacion)
 
 class PoaAdmin(admin.ModelAdmin):
     list_display = ('id','anio','indicadorPedi','NumeroSeguimiento','totalAnio','pro1','pro2')
-    list_filter = ('NumeroSeguimiento','anio')
+    list_filter = ('NumeroSeguimiento','anio','indicadorPedi.entidadResponsable')
 admin.site.register(Poa, PoaAdmin)
 
 
